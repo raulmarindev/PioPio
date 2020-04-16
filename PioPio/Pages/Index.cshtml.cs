d08e89e5ea01793180a7c0356ac97dd1af4ec64a
Raúl Marín <rmarin82@gmail.com>
2020-04-16 20:24:38 +0200

    Initial commit.

---
+++ PioPio/Pages/Index.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using LinqToTwitter;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace PioPio.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly TwitterContext _twitterContext;
+
+        public IndexModel(ILogger<IndexModel> logger, TwitterContext twitterContext)
+        {
+            _logger = logger;
+            _twitterContext = twitterContext;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ PioPio/TwitterContextServiceCollection.cs
@@ -0,0 +1,14 @@
+﻿namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class TwitterContextServiceCollection
+    {
+        public static IServiceCollection AddTwitterContext(this IServiceCollection services)
+        {
+            //var context = new TwitterContext
+            //{
+            //};
+
+            return services;
+        }
+    }
+}
